92f9b97553f1969dbb4f2b6230826c6547777568
Joel Lisboa <joelisboas@gmail.com>
2018-10-18 01:27:41 -0300

    commit

---
+++ Atividade5/js/js.js
@@ -0,0 +1,26 @@
+let botao = document.querySelector(".botao_sanduiche");
+let lateral = document.querySelector(".nav_lateral");
+let fole = document.querySelectorAll(".fole");
+let conteudo = document.querySelectorAll(".conteudo");
+
+function mostrarMenu() {
+    lateral.classList.toggle("show");
+}
+botao.addEventListener("click", mostrarMenu);
+
+// ------------------------------------------------
+
+function mostrarConteudo(i){
+    conteudo[i].classList.toggle("show");
+    for(let j=0; j < conteudo.length; j++) {
+        if(j != i)
+            conteudo[j].classList.remove("show");
+    }
+}
+
+for(let i=0; i < conteudo.length; i++)
+    fole[i].addEventListener("click", function(){
+        mostrarConteudo(i);
+    });
+
+

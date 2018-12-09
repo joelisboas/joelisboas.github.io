2db777ef601ab8c063206ed33b7f8ba4e109e5f6
Joel Lisboa <joelisboas@gmail.com>
2018-12-08 22:42:48 -0200

    commit

--- Trabalho2/js/js.js
+++ Trabalho2/js/js.js
@@ -1,46 +1,4 @@
-
-function criarGrupos(){
-    let grupos = document.querySelector(".ul_grupos");
-    let grupo = document.createElement("li");
-    grupos.appendChild(grupo);
-
-    let foto_grupo = document.createElement("div");
-    foto_grupo.classList.add("foto_grupo");
-    grupo.appendChild(foto_grupo);
-
-    let nome_grupo = document.createElement("span");
-    let texto_nome = document.createTextNode("Nome do grupo");
-    nome_grupo.appendChild(texto_nome);
-    grupo.appendChild(nome_grupo);
-}
-
-criarGrupos();
-criarGrupos();
-criarGrupos();
-// function postGrupos(){
-//     let xhttp = new XMLHttpRequest();
-
-//     xhttp.onreadystatechange = function(){
-//       if(this.readyState == 4){
-//         console.log("POST entrou");
-//         console.log(JSON.parse(xhttp.responseText));
-//       }
-//     }
 let url = "http://rest.learncode.academy/api/joelisboas/lms";
-function getGrupos(){
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function(){
-        if(xhttp.readyState==4){
-            console.log("GET");
-            let body_parsed = JSON.parse(xhttp.responseText);
-            console.log(xhttp.responseText);
-            console.log(body_parsed);
-            // console.log(body_parsed[0].nome);
-        }
-    }
-    xhttp.open("GET", url, true);
-    xhttp.send();
-}
 
 function postGrupos(){
     let xhttp1 = new XMLHttpRequest();
@@ -51,6 +9,9 @@ function postGrupos(){
         }
     }
     xhttp1.open("POST", url, true);
+    xhttp1.onload = function(){
+        criarGrupos();
+      };
     xhttp1.setRequestHeader("Content-type","application/json");
     let msg = [
         //GRUPO 1
@@ -86,43 +47,54 @@ function postGrupos(){
             texto: "Que bom"
             },
         ]
-        },
-        //GRUPO 3
-        { 
-        grupo: "Só topzera",
-        mensagens: [{
-            usuario: "victor03",
-            texto: "Bom?"
-            },
-            {
-            usuario: "robson_alves",
-            texto: "Bom"
-            },
-            {
-            usuario: "victor03",
-            texto: "Que bom"
-            },
-        ]
-        },
-        { 
-        grupo: "aaaaaaaaaaaaaaaaaaaaaaa",
-        mensagens: [{
-            usuario: "victor03",
-            texto: "Bom?"
-            },
-            {
-            usuario: "robson_alves",
-            texto: "Bom"
-            },
-            {
-            usuario: "victor03",
-            texto: "Que bom"
-            },
-        ]
         }
     ];
     xhttp1.send(JSON.stringify(msg));
 }
 
+function getGrupos(){
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(xhttp.readyState==4){
+            console.log("GET");
+            let body_parsed = JSON.parse(xhttp.responseText);
+            console.log(xhttp.responseText);
+            console.log(body_parsed);
+            // console.log(body_parsed[0].nome);
+        }
+    }
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+function criarGrupos(){
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(xhttp.readyState==4){
+            console.log("CRIANDO GRUPO");            
+            function nome_do_grupo(nome){
+                let grupos = document.querySelector(".ul_grupos");
+                let grupo_linha = document.createElement("li");
+                grupos.appendChild(grupo_linha);
+            
+                let foto_grupo = document.createElement("div");
+                foto_grupo.classList.add("foto_grupo");
+                grupo_linha.appendChild(foto_grupo);
+
+                let nome_grupo = document.createElement("span");
+                let texto_nome = document.createTextNode(nome);
+                nome_grupo.appendChild(texto_nome);
+                grupo_linha.appendChild(nome_grupo);
+            }
+            let body_parsed = JSON.parse(xhttp.responseText);
+            let grupo0 = body_parsed[0][0].grupo;
+            let grupo1 = body_parsed[0][1].grupo;
+            nome_do_grupo(grupo0);
+            nome_do_grupo(grupo1);
+        }
+    }
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
 postGrupos();
-getGrupos();
